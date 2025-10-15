f9d84cd82043a3d8acd4cc58731db44ef83f3766
pbrunet <brunetp21@gmail.com>
2025-10-15 17:09:18 -0400

    Add legend in JS

--- day1.py
+++ day1.py
@@ -1,20 +1,9 @@
 import folium
-from folium.plugins import MarkerCluster
 import geopandas as gpd
-
 from constants import DATA_PATH, OUTPUT_DIR
 
 # Params
 day = "1"
-
-api_jawg = DATA_PATH / "API_jawg.txt"  
-
-try:
-    with open(api_jawg, 'r') as file:
-        JAWG_TOKEN = file.readline()
-except FileNotFoundError:
-    print(f"Error: The file '{api_jawg}' was not found.")
-    
 
 # Load cafés
 cafes = gpd.read_file(DATA_PATH / "cafe_montreal.geojson")
@@ -24,36 +13,55 @@ cafes = cafes[
 ].copy()
 cafes["name"] = cafes["name"].fillna("Café sans nom")
 
-# Create base map
+# Create the map
 m = folium.Map(
     location=[45.5017, -73.5673],
     zoom_start=13,
-    min_zoom=11,  
+    min_zoom=11,
     max_zoom=18,
-    tiles="https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png",
+    tiles="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all//{z}/{x}/{y}.png",
     attr="© OpenStreetMap, © CartoDB",
-
 )
 
-# Add bean markers
-icon_path = DATA_PATH/"coffee.png"
+icon_path = DATA_PATH / "coffee.png"
 
+# Add beans
 for _, row in cafes.iterrows():
     lat, lon = row.geometry.y, row.geometry.x
     name = row["name"]
-    icon = folium.CustomIcon(str(icon_path), icon_size=(8, 8), icon_anchor=(10, 10))
-    folium.Marker(
+
+    icon = folium.CustomIcon(str(icon_path), icon_size=(10, 10), icon_anchor=(5, 5))
+    marker = folium.Marker(
         location=[lat, lon],
         popup=f"<b>{name}</b>",
         icon=icon
-    ).add_to(m)
+    )
+    marker.add_to(m)
 
-# Fit to bounds
+# Add legend
+legend_html = f"""
+<div id='legend' style="
+    position: fixed; 
+    bottom: 100px; left: 100px; 
+    background-color: white; 
+    border: 2px solid lightgray; 
+    border-radius: 10px; 
+    padding: 8px 12px; 
+    box-shadow: 2px 2px 6px rgba(0,0,0,0.3);
+    font-size: 14px;
+    z-index: 9999;
+">
+    <img src='{icon_path.as_posix()}' style="width:10px; height:10px; vertical-align:middle; margin-right:6px;">
+    <b>Coffee Place</b>
+</div>
+"""
+m.get_root().html.add_child(folium.Element(legend_html))
+
 bounds = cafes.total_bounds
 m.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
 m.options['maxBounds'] = [[bounds[1], bounds[0]], [bounds[3], bounds[2]]]
 
-# Add layer control
+# Control layers
 folium.LayerControl().add_to(m)
 
 # Save
@@ -61,4 +69,4 @@ output_dir = OUTPUT_DIR / f"day_{day}"
 output_dir.mkdir(parents=True, exist_ok=True)
 m.save(output_dir / "montreal_cafes.html")
 
-
+print(f"Map saved : {output_dir / 'montreal_cafes.html'}")
